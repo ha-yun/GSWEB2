7c24a94c1afae240b14e493486aadef59b195ac2
yun-aha <deu05230@naver.com>
2021-07-22 10:21:50 +0900

    2021/07/22 gsweb2! class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
@@ -16,11 +17,9 @@ from accountapp.models import HelloWorld
 def ha_world(request):
     if request.method == 'POST':
         temp = request.POST.get('hello_world_input')
-
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-
         return HttpResponseRedirect(reverse('accountapp:ha_world'))
 
     else:
@@ -40,16 +39,18 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+has_ownership =[login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
     template_name = 'accountapp/update.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
