fe1fc4fa889e7d97cfbc009385f2a4adce8a24e8
yun-aha <deu05230@naver.com>
2021-08-26 09:28:53 +0900

    2021/08/26 gsweb2! class commit 1

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -12,18 +13,20 @@ from likeapp.models import LikeRecord
 
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
-
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-
         like_record = LikeRecord.objects.filter(user=user, article=article)
         if like_record.exists():
+            # 좋아요가 반영 x
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능!')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
         article.like += 1
         article.save()
+        # 좋아요가 반영 o
+        messages.add_message(request, messages.SUCCESS, '좋아요><')
         return super().get(request, *args, **kwargs)
 
     def  get_redirect_url(self, *args, **kwargs):
